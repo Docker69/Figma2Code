0495dfc4b873eba9841108bd01a47bbbed7f8299
Docker69 <>
2023-04-18 19:23:34 +0000

    Making it more responsive

--- src/pages/ChatPage.tsx
+++ src/pages/ChatPage.tsx
@@ -2,15 +2,17 @@ import { FunctionComponent } from "react";
 import Sidebar from "../components/Sidebar";
 import AppBarNavigation from "../components/AppBarNavigation";
 import Main from "../components/Main";
+import Footer from "../components/Footer";
 import styles from "./ChatPage.module.css";
 
 const ChatPage: FunctionComponent = () => {
   return (
     <div className={styles.chatpage}>
       <Sidebar />
-      <div className={styles.appBarNavigationParent}>
+      <div className={styles.secondcolumn}>
         <AppBarNavigation />
         <Main />
+        <Footer />
       </div>
     </div>
   );
