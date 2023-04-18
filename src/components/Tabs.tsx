a14d29c89d11c1f07340fcce89492b8a0018f69b
Docker69 <>
2023-04-18 18:26:20 +0000

    Pushed from Locofy Builder

--- src/components/Tabs.tsx
+++ src/components/Tabs.tsx
@@ -5,23 +5,25 @@ import styles from "./Tabs.module.css";
 const Tabs: FunctionComponent = () => {
   return (
     <div className={styles.tabs}>
-      <div className={styles.tab}>
+      <Button className={styles.tab} variant="text" color="primary">
+        Tab
+      </Button>
+      <div className={styles.tab1}>
         <div className={styles.container}>
           <div className={styles.icon}>
             <img
               className={styles.starfilledIcon}
               alt=""
-              src="/starfilled.svg"
+              src="/starfilled1.svg"
             />
           </div>
           <Button className={styles.button} variant="text" color="primary">
             Tab
           </Button>
         </div>
-        <div className={styles.line} />
       </div>
       <div className={styles.tab1}>
-        <div className={styles.container1}>
+        <div className={styles.container}>
           <div className={styles.icon}>
             <img
               className={styles.starfilledIcon}
@@ -35,7 +37,7 @@ const Tabs: FunctionComponent = () => {
         </div>
       </div>
       <div className={styles.tab1}>
-        <div className={styles.container1}>
+        <div className={styles.container}>
           <div className={styles.icon}>
             <img
               className={styles.starfilledIcon}
@@ -49,7 +51,7 @@ const Tabs: FunctionComponent = () => {
         </div>
       </div>
       <div className={styles.tab1}>
-        <div className={styles.container1}>
+        <div className={styles.container}>
           <div className={styles.icon}>
             <img
               className={styles.starfilledIcon}
@@ -63,21 +65,7 @@ const Tabs: FunctionComponent = () => {
         </div>
       </div>
       <div className={styles.tab1}>
-        <div className={styles.container1}>
-          <div className={styles.icon}>
-            <img
-              className={styles.starfilledIcon}
-              alt=""
-              src="/starfilled1.svg"
-            />
-          </div>
-          <Button className={styles.button} variant="text" color="primary">
-            Tab
-          </Button>
-        </div>
-      </div>
-      <div className={styles.tab1}>
-        <div className={styles.container1}>
+        <div className={styles.container}>
           <div className={styles.icon}>
             <img
               className={styles.starfilledIcon}
